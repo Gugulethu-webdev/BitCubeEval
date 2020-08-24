744a5e7f7c02de39693493d0c51c0ffee1920856
Gugulethu-webdev <mtshaliamo@gmail.com>
2020-08-24 22:59:30 +0200

    Updates

---
+++ precache-manifest.914fc22373c79b1a8ba2461941be0623.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a24ba1c0fa6fd56c882854ebd88052b5",
+    "url": "/BitcubeEval/index.html"
+  },
+  {
+    "revision": "edc7a1650b1ce2a8ec8a",
+    "url": "/BitcubeEval/static/css/main.903dbdbb.chunk.css"
+  },
+  {
+    "revision": "1127d82672466b3cf6f2",
+    "url": "/BitcubeEval/static/js/2.8be8b1b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BitcubeEval/static/js/2.8be8b1b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edc7a1650b1ce2a8ec8a",
+    "url": "/BitcubeEval/static/js/main.c60fab6b.chunk.js"
+  },
+  {
+    "revision": "73999be06d3d40b613f3",
+    "url": "/BitcubeEval/static/js/runtime-main.1364bf71.js"
+  }
+]);
